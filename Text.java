362100fa85750485dfdb6f7e9174d4bf692bb943
tybatt <tbattaglia@csumb.edu>
2014-03-10 21:19:11 -0700

    Test
    Testing commits

---
+++ Text.java
@@ -0,0 +1,10 @@
+public class Text {
+
+  public static void main(String[] args) {
+
+	System.out.println("\u001B[31mHello World!\033[0m");
+	System.out.println("\u001B[34mhello world!\033[0m");
+
+  }
+
+} 
